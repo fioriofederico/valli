81e3e8f899c67b69af054a78b2a51c02bc865f94
Federico Fiorio <federico.fiorio@outlook.it>
2022-01-20 15:19:09 +0100

    Modificato e finito con il conteggio

--- bin/valli.dart
+++ bin/valli.dart
@@ -1,17 +1,25 @@
 void main() {
   const minSize = 2;
   const maxSize = 1000000;
-  var passi = 8;
-  var percorso = 'UDDDUDUU';
+  var passi = 58;
+  var percorso = 'UUUUDDDUUUDDDDDDDDUUUUUUDDDDDDDDDDDDDDDDUUUUUUUUUUUUUUUUUU';
   var stringSize = percorso.length;
+  var movimento = 0;
+  var numValliPercorsi = 0;
   if ((stringSize <= minSize) || (stringSize >= maxSize)) {
     print(
         'La lunghezza della stringa inserita non rispetta le condizioni inizili');
   } else {
-    print(percorso);
-    print(stringSize);
     for(var i=0 ; i < stringSize; i++){
-      print(percorso);
+      if(percorso[i] == 'U'){
+        movimento ++;
+      }else{
+        movimento --;
+      }
+      if(movimento < 0 && !miTrovoNellavalle){
+        numValliPercorsi++;
+      }
     }
+    print(numValliPercorsi);
   }
 }
